78a9af11cbfd70bc6e1c65fd28a945392333a9ad
hhh <aaa@bbb.ccc>
2015-07-24 00:17:02 +0800

    jbjkhk

--- app/src/main/java/com/czs/com/package_chanell/MainActivity.java
+++ app/src/main/java/com/czs/com/package_chanell/MainActivity.java
@@ -12,7 +12,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //HistoryTest
-        //why this can not been seen
+        //why this can not been seennjnjnjnj
+        //nooooooooooooooooooooooooooooooooooooooooooooooooooooooo
+
     }
 
     @Override

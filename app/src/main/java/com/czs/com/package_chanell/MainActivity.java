51291a998e6d164ca7c9f7ccc3cc38a79b42e50a
aaa <639956245@qq.com>
2015-07-23 23:57:26 +0800

    historyis

--- app/src/main/java/com/czs/com/package_chanell/MainActivity.java
+++ app/src/main/java/com/czs/com/package_chanell/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //HistoryTest
+        //rrr
         //why this can not been seen
     }
 
